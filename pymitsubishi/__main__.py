17a3cd87d11f4b1ef583cd21814fae692b02b0ba
Niobos <niobos@dest-unreach.be>
2025-08-06 22:07:25 +0200

    Add verbosity to __main__

--- pymitsubishi/__main__.py
+++ pymitsubishi/__main__.py
@@ -1,10 +1,16 @@
 import argparse
+import logging
 
 from pymitsubishi import MitsubishiController
 
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--verbose', '-v', help="Verbose output", action="count", default=0)
 parser.add_argument("host", help="Hostname or IP address to connect to, optionally followed by ':port'")
 args = parser.parse_args()
+
+logging.basicConfig(level=logging.WARNING - 10 * args.verbose)
+logger = logging.getLogger(__name__)
 
 ctrl = MitsubishiController.create(args.host)
 ctrl.fetch_status()
